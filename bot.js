5be69cde00ed2ebb0a65db4432f57a4d02829f19
rowakir <rowa.kir1@gmail.com>
2026-02-17 15:29:45 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
-
-const token = process.env.BOT_TOKEN;
-
+const token = 'YOUR_BOT_TOKEN'; // BotFather থেকে পাওয়া token
 const bot = new TelegramBot(token, { polling: true });
 
+// Start command
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Play X.Snake 🐍", {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Play X.Snake!", {
     reply_markup: {
       inline_keyboard: [
         [
-          {
-            text: "Play Game",
-            web_app: { url: "https://rowakir.github.io/snake-evolution-game/" }
-          }
+          { text: "Play X.Snake", web_app: { url: "https://rowakir.github.io/snake-evolution-game/" } }
         ]
       ]
     }
   });
 });
 
+// Optional: Inline Mode
+bot.on('inline_query', (query) => {
+  const results = [
+    {
+      type: 'article',
+      id: '1',
+      title: 'Play X.Snake!',
+      input_message_content: { message_text: '🚀 Tap below to play X.Snake!' },
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Play X.Snake", web_app: { url: "https://rowakir.github.io/snake-evolution-game/" } }]
+        ]
+      }
+    }
+  ];
+  bot.answerInlineQuery(query.id, results);
+});
